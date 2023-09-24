04a4e415645a78e90d6b7e58eed10b00770d34a2
e-mustafa <eng.mustafax@gmail.com>
2023-09-24 22:44:59 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const routes = require('./routes');
+
+app.use(
+	cors({
+		origin: ['http://localhost:3000', 'http://127.0.0.1:3000/'],
+		credentials: true,
+		allowedHeaders: 'Content-Type,Authorization',
+	})
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api', routes);
+app.use('/api/blog', express.static('./uploads'));
+app.use('/api/user', express.static('./uploads'));
+
+// to create random code
+// const crypto = require("crypto")
+// console.log(crypto.randomBytes(32).toString("hex"));
+
+port = process.env.Port || 5000;
+const url = process.env.DB_CONNECTION_URL;
+
+mongoose
+	.connect(url, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('DataBase Connected'))
+	.catch((err) => console.log(err.message));
+
+// app.all('*', (req, res) => {
+// 	/* 404 handling code */
+// });
+
+app.listen(port, () => console.log('app listen on port ' + port));
